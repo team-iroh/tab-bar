2340c5864136ecc8a459c9b89487f4c277bd26b3
zjayers <zayers.dev@gmail.com>
2020-06-29 19:08:00 -0700

    init

--- client/src/components/Tabbar/TabBar.style.js
+++ client/src/components/Tabbar/TabBar.style.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   width: 100%;
   height: 6.6rem;
   color: #282828;
-  background-color: #e8e8e8;
+  background-color: #fff;
   border-color: #e8e8e8;
   border-bottom: 0.1rem solid #dcdedd;
   display: flex;
